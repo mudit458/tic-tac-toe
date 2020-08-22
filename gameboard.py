fb2d5d95f49c525c6873264ffde810938bcd5ebc
mudit <mksingh458@gmail.com>
2020-08-23 03:16:39 +0530

    Revert "everything works as of now"
    This reverts commit ec228a1e

--- gameboard.py
+++ gameboard.py
@@ -1,7 +1,6 @@
 import threading
-from random import random
 from tkinter import *
-from tkinter import messagebox
+from tkinter import messagebox, simpledialog
 
 
 def promptInput(message):
@@ -30,8 +29,7 @@ class Game:
         self.con = ""
         self.user_name = ""
         while self.user_name == "":
-            # self.user_name = self.getName().strip()
-            self.user_name = "abc"
+            self.user_name = self.getName().strip()
         self.myTurn = False
         self.xy = ""
         self.other_player = ""
@@ -87,7 +85,7 @@ class Game:
             self.wins += 1
         elif res == "loss":
             self.loss_count += 1
-        elif res == "tie":
+        elif res == "draw":
             self.ties_count += 1
 
     def resetGameBoard(self):  # Resets the game
@@ -95,31 +93,26 @@ class Game:
             for j in range(3):
                 self.b[i][j]["text"] = ""
                 self.b[i][j]["state"] = NORMAL
-        self.last_player = ""
-        self.printStatus()
 
     def isWinner(self):
         # horizontal
-        for char in ("X", "O"):
-            rows = [0] * 3
-            cols = [0] * 3
-            for i in range(3):
-                for j in range(3):
-                    if self.b[i][j]["text"] == char:
-                        rows[i] += 1
-                        cols[j] += 1
-            if max(rows + cols) == 3:
-                return char
-            ldiag = 0
-            rdiag = 0
-            for i in range(3):
-                if self.b[i][i]["text"] == char:
-                    ldiag += 1
-                if self.b[i][3 - 1 - i]["text"] == char:
-                    rdiag += 1
-            if max(ldiag, rdiag) >= 3:
-                return char
-        return None
+        rows = [0] * 3
+        cols = [0] * 3
+        for i in range(3):
+            for j in range(3):
+                if self.b[i][j]["text"] == self.player_char:
+                    rows[i] += 1
+                    cols[j] += 1
+        if max(rows + cols) == 3:
+            return True
+        ldiag = 0
+        rdiag = 0
+        for i in range(3):
+            if self.b[i][i]["text"] == self.player_char:
+                ldiag += 1
+            if self.b[i][3 - 1 - i]["text"] == self.player_char:
+                rdiag += 1
+        return max(ldiag, rdiag) >= 3
 
     def enableAll(self):
         for i in range(3):
@@ -136,31 +129,27 @@ class Game:
         cnt = 0
         for i in range(3):
             for j in range(3):
-                if self.b[i][j]["text"] in ('X', 'O'):
+                if self.b[i][j]["text"] != '':
                     cnt += 1
-        if cnt == 9:
-            return True
-        return False
+        return cnt == 9
 
     def printStatus(self):
-        text = "Statistics\n"
+        text="Statistics\n"
         text += f"Name: {self.user_name}\n Last Player: {self.last_player}\n"
         text += f"Number of games played: {self.games_played}\nWins: {self.wins}\n"
         text += f"Number of ties: {self.ties_count}\nNumber of losses: {self.loss_count}"
-        self.stats["text"] = text
-        self.root.update()
 
     def click(self, row=-1, col=-1):
+        self.b[row][col].config(state=DISABLED, disabledforeground=self.colour[self.player_char])
         self.xy = str(row) + " " + str(col)
         self.disableAll()
         if self.myTurn:
-            self.b[row][col].config(state=DISABLED, disabledforeground=self.colour[self.player_char])
             self.b[row][col].config(text=self.player_char)
-            self.root.update()
             self.con.send(f"{row} {col}".encode('utf-8'))
             print("sent ", row, col)
         else:
             print("waiting for move")
+            other = ""
             if self.player_char == "X":
                 other = "O"
             else:
@@ -168,68 +157,29 @@ class Game:
             # get other player's move
             print("waiting for other player to make a move")
             xy = self.con.recv(2048).decode('utf-8')
-            if xy == "quit":
-                print("other Player Quit")
-                self.root.destroy()
-                sys.exit()
             print("other player moved at", xy)
             row, col = map(int, xy.split())
             self.b[row][col].config(text=other)
-            self.b[row][col].config(state=DISABLED, disabledforeground=self.colour[other])
-        # check win
-        res = self.isWinner()
-        if res:
-            if res == self.player_char:
-                winner = self.user_name
-                self.updateGamesPlayed('win')
-            else:
-                winner = self.other_player
-                self.updateGamesPlayed('loss')
-            messagebox.Message(message=f"{winner} won")
-            self.resetGameBoard()
-            self.printStatus()
+        #check win
 
-            return
-
-        # check draw
-        res = self.boardIsFull()
-        if res:
-            messagebox.Message("The game resulted in a draw")
-            self.updateGamesPlayed('tie')
-            messagebox.Message(message="The game tied")
-            self.printStatus()
-            self.resetGameBoard()
-            self.printStatus()
-
+        #check draw
 
         self.myTurn = not self.myTurn
-
-        if self.myTurn:
-            self.label.config(text=f"{self.user_name}' Turn")
-            self.last_player = self.user_name
-        else:
-            self.label.config(text=f"{self.other_player}'s Turn")
-            self.last_player = self.other_player
-        self.printStatus()
-        self.root.update()
 
         if self.myTurn:
             self.enableAll()
         else:
             self.click()
 
+
     def close_window(self):
-        if self.myTurn:
-            self.con.send("quit".encode('utf-8'))
         self.root.destroy()
 
     def start_game_window(self):
         mainloop()
 
     def play(self):
-        if self.myTurn:
-            self.label["text"] = f"{self.user_name}' Turn"
-        else:
-            self.label["text"] = f"{self.other_player}'s Turn"
+        if not self.myTurn:
+            self.root.after(2000, self.click)
         self.start_game_window()
 
